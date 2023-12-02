608b175c053f628e68ac999c209423bc0bc42168
antoniluthfi <luthfiantoni027@gmail.com>
2023-12-02 15:39:00 +0800

    create user model & migration

---
+++ migrations/20231201125707-create-user.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      phone_number: {
+        type: Sequelize.STRING
+      },
+      profile_image: {
+        type: Sequelize.STRING
+      },
+      role: {
+        type: Sequelize.ENUM('user', 'admin', 'event organizer')
+      },
+      status: {
+        type: Sequelize.ENUM('active', 'inactive')
+      },
+      otp: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
---
+++ models/user.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    phone_number: DataTypes.STRING,
+    profile_image: DataTypes.STRING,
+    role: DataTypes.ENUM('user', 'admin', 'event organizer'),
+    status: DataTypes.ENUM('active', 'inactive'),
+    otp: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'users'
+  });
+  return User;
+};
