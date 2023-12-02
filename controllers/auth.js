1a59578170d40009e9b4f2d3d6f3a9ee0ff17e02
antoniluthfi <luthfiantoni027@gmail.com>
2023-12-02 15:39:30 +0800

    create auth functionalities

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,30 @@
+const models = require("../models");
+
+const checkRegisteredEmail = async (req, res) => {
+  try {
+    const user = await models.User.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (user) {
+      return res.status(200).json({
+        success: true,
+        message: "Email terdaftar",
+      });
+    } else {
+      return res.status(404).json({
+        success: false,
+        message: "Email tidak terdaftar",
+      });
+    }
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
 const login = async (req, res) => {
   return res.status(200).json({
     success: true,
@@ -6,5 +33,6 @@ const login = async (req, res) => {
 };
 
 module.exports = {
-  login
-}
+  checkRegisteredEmail,
+  login,
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const { login } = require("../controllers/auth");
+const { login, checkRegisteredEmail } = require("../controllers/auth");
+const { body } = require("express-validator");
 const router = express.Router();
 
-router.get('/login', login);
+router.post(
+  "/check-email",
+  [body("email").notEmpty().withMessage("Email harus diisi!")],
+  checkRegisteredEmail
+);
+router.get("/login", login);
 
 module.exports = router;
